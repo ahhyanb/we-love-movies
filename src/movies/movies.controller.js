617c3b42d6b91affd921c605f68f3d68beb2b8aa
Ryan Bartolome <rahhyahh@icloud.com>
2024-11-11 15:36:47 -1000

    added query to list movie by ID

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -1,15 +1,20 @@
 const moviesService = require("./movies.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
+
 async function list(req, res, next) {
     const data = await moviesService.list();
     res.json({ data });
 }
 
 async function listMoviesShowing(req, res, next) {
-    const data = await moviesService.listMoviesShowing();
+    const isShowing = req.query.is_showing === "true";
+    const data = isShowing
+        ? await moviesService.listMoviesShowing() // ternary operator
+        : await moviesService.list();
     res.json({ data });
 }
+
 
 module.exports = {
     list: asyncErrorBoundary(list),
